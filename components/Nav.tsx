c24cb990ee84ae58b228849e7d001abe2498cc6c
Tahara Kazuki <>
2024-10-03 16:30:22 +0900

    update

---
+++ components/Nav.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { NAV_LINKS } from '@/constant';
+
+const Nav = () => {
+  const pathname = usePathname();
+  return (
+    <nav className="flex gap-8 text-center capitalize">
+      {NAV_LINKS.map((link, i) => (
+        <Link
+          href={link.href}
+          key={i}
+          className={`${pathname === link.href && 'border-b-2 border-accent'}`}
+        >
+          {link.name}
+          <span className="text-[10px]">{link.subText}</span>
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ constant/index.ts
@@ -0,0 +1,7 @@
+export const NAV_LINKS = [
+  { href: '/', name: 'Home', subText: 'ホーム' },
+  { href: '/about', name: 'About', subText: '私たちについて' },
+  { href: '/treatments', name: 'Schedule', subText: 'スケジュール' },
+  { href: '/treatments', name: 'Questions', subText: 'よくある質問' },
+  { href: '/contact', name: 'Contact', subText: 'お問い合わせ' },
+];
