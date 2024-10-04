d022358bf9878038dac78ee857c5bbf72c4f2e4c
Tahara Kazuki <>
2024-10-04 11:06:31 +0900

    update

--- components/NavList.tsx
+++ components/NavList.tsx
@@ -1,24 +1,61 @@
 'use client';
 
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import { NAV_LINKS } from '@/constant';
+import { motion } from 'framer-motion';
+
+const letterAnim = {
+  initial: {
+    y: '100%',
+    opacity: 0,
+  },
+  enter: (i: number[]) => ({
+    y: 0,
+    opacity: 1,
+    transition: { duration: 1, ease: [0.6, 0, 0.2, 1], delay: i[0] },
+  }),
+  exit: (i: number[]) => ({
+    y: '100%',
+    opacity: 0,
+    transition: { duration: 0.8, ease: [0.6, 0, 0.2, 1], delay: i[1] },
+  }),
+};
+
+const getLetter = (name: string) => {
+  const letters = [] as JSX.Element[];
+  name.split('').forEach((letter, index) => {
+    letters.push(
+      <motion.span
+        variants={letterAnim}
+        initial="initial"
+        animate="enter"
+        exit="exit"
+        custom={[index * 0.04, (name.length - index) * 0.01]}
+        key={index}
+      >
+        {letter}
+      </motion.span>
+    );
+  });
+  return letters;
+};
 
 const Nav = () => {
-  const pathname = usePathname();
   return (
-    <nav className="flex gap-8 text-center capitalize">
-      {NAV_LINKS.map((link, i) => (
-        <Link
-          href={link.href}
-          key={i}
-          className={`${pathname === link.href && 'border-b-2'}`}
-        >
-          {link.name}
-          <span className="text-[10px]">{link.subText}</span>
-        </Link>
+    <ul className="flex flex-col items-center gap-8 font-primary text-4xl font-semibold uppercase text-accent">
+      {NAV_LINKS.map((link, index) => (
+        <li key={index} className="flex">
+          <Link
+            href={link.href}
+            key={index}
+            className="flex items-baseline overflow-hidden transition-all xl:hover:text-white"
+          >
+            {getLetter(link.name)}
+            <span className="ml-3 text-xs">{getLetter(link.subText)}</span>
+          </Link>
+        </li>
       ))}
-    </nav>
+    </ul>
   );
 };
 
