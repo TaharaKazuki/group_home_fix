cbf6ac812e57725e22c28e34c686b200e68c901a
Tahara Kazuki <>
2024-10-07 09:40:34 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
 'use client';
 
+import Hero from '@/components/Hero';
+import { useEffect } from 'react';
+
 export default function Home() {
+  useEffect(() => {
+    const loadLocomotiveScroll = async () => {
+      const LocomotiveScroll = (await import('locomotive-scroll')).default;
+      new LocomotiveScroll();
+    };
+
+    loadLocomotiveScroll();
+  }, []);
+
   return (
     <div className="h-full overflow-hidden">
-      aaaa
+      <Hero />
       <div className="h-[4000px]" />
     </div>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,1 +1,17 @@
-  
+const Hero = () => {
+  return (
+    <section className="relative h-[100vh] text-white xl:h-screen bg-red-500">
+      <div className="container relative mx-auto flex h-full flex-col items-center xl:flex-row">
+        <div
+          data-scroll
+          data-scroll-speed="0.4"
+          className="flex h-full flex-1 flex-col items-center justify-center gap-4 text-center xl:gap-10 xl:pb-12"
+        >
+          Sample title
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
