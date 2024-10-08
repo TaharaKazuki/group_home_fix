628196739c47038e1965e45acbaf71246936ab03
Tahara Kazuki <>
2024-10-08 10:42:29 +0900

    update

--- app/questions/page.tsx
+++ app/questions/page.tsx
@@ -1,58 +1,20 @@
-'use client';
-
-import { useState } from 'react';
-import { motion } from 'framer-motion';
 import { FAQ_LIST } from '@/constant';
-
-interface AccordionItemProps {
-  question: string;
-  answer: string;
-}
-
-const AccordionItem = ({ question, answer }: AccordionItemProps) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleAccordion = () => setIsOpen(!isOpen);
-
-  const variants = {
-    open: { height: 'auto', opacity: 1 },
-    closed: { height: 0, opacity: 0 },
-  };
-
-  return (
-    <div className="border-b border-gray-300 border-dotted">
-      <button
-        onClick={toggleAccordion}
-        className="w-full text-left py-4 font-semibold focus:outline-none"
-      >
-        {question}
-      </button>
-      <motion.div
-        initial="closed"
-        animate={isOpen ? 'open' : 'closed'}
-        variants={variants}
-        transition={{ duration: 0.3 }}
-        className="overflow-hidden"
-      >
-        <p className="py-4">{answer}</p>
-      </motion.div>
-    </div>
-  );
-};
+import { AccordionItem } from '@/components/ui/accordionItem';
 
 const QuestionsPage = () => {
   return (
     <main className="pt-[10vw] container mx-auto">
       <p className="font-semibold text-2xl">
-        Equipments
-        <span className="block text-xs font-normal mt-2">設備など</span>
+        Questions
+        <span className="block text-xs font-normal mt-2">よくある質問</span>
       </p>
       <section className="my-8">
-        {FAQ_LIST.map((item, index) => (
+        {FAQ_LIST.map((item, i) => (
           <AccordionItem
-            key={index}
+            key={i}
             question={item.question}
             answer={item.answer}
+            questionId={i + 1}
           />
         ))}
       </section>
---
+++ components/ui/accordionItem.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import {
+  IoIosArrowDropdownCircle,
+  IoIosArrowDropupCircle,
+} from 'react-icons/io';
+
+type AccordionItemProps = {
+  question: string;
+  answer: string;
+  questionId: number;
+};
+
+export const AccordionItem = ({
+  question,
+  answer,
+  questionId,
+}: AccordionItemProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = () => setIsOpen(!isOpen);
+
+  const variants = {
+    open: { height: 'auto', opacity: 1 },
+    closed: { height: 0, opacity: 0 },
+  };
+
+  return (
+    <div className="border-b border-gray-300 border-dotted">
+      <button
+        onClick={toggleAccordion}
+        className="w-full text-left py-4 text-xs lg:text-base focus:outline-none flex flex-row items-center justify-start gap-2"
+      >
+        <p className="text-red-300 font-semibold">Q.{questionId}</p>
+        {question}
+        {isOpen ? (
+          <IoIosArrowDropupCircle size={20} className="text-red-300" />
+        ) : (
+          <IoIosArrowDropdownCircle size={20} className="text-red-300" />
+        )}
+      </button>
+      <motion.div
+        initial="closed"
+        animate={isOpen ? 'open' : 'closed'}
+        variants={variants}
+        transition={{ duration: 0.3 }}
+        className="overflow-hidden"
+      >
+        <p className="lg:py-4 py-2">{answer}</p>
+      </motion.div>
+    </div>
+  );
+};
