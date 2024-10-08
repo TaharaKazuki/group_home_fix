4c0f44120c4cf7a9f35958efa37a97cb0bfa97a6
Tahara Kazuki <>
2024-10-08 13:14:47 +0900

    update

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -36,7 +36,7 @@ const SchedulePage = () => {
               <h2 className="lg:text-2xl text-lg text-center lg:text-left font-semibold">
                 WALK
               </h2>
-              <p className="pt-10 lg:leading-10 leading-6 text-center lg:text-left lg:text-base text-sm">
+              <p className="pt-5 lg:pt-10 lg:leading-10 leading-6 text-center lg:text-left lg:text-base text-sm">
                 サンプル文章が入ります
                 <br />
                 サンプル文章が入りますサンプル文章が入ります
@@ -57,7 +57,112 @@ const SchedulePage = () => {
               <h2 className="lg:text-2xl text-lg text-center lg:text-left font-semibold">
                 WALK
               </h2>
-              <p className="pt-10 lg:leading-10 leading-6 text-center lg:text-left lg:text-base text-sm">
+              <p className="pt-5 lg:pt-10 lg:leading-10 leading-6 text-center lg:text-left lg:text-base text-sm">
+                サンプル文章が入ります
+                <br />
+                サンプル文章が入りますサンプル文章が入ります
+                <br />
+                サンプル文章が入りますサンプル文章が入ります
+                <br />
+                サンプル文章が入ります。
+              </p>
+            </div>
+            <img
+              src="/assets/hero/hero_background_1.png"
+              alt="sample"
+              className="size-full object-cover aspect-square"
+            />
+          </div>
+          <div className="grid w-full gap-[10vw] grid-cols-1 lg:grid-cols-2">
+            <div>
+              <h2 className="lg:text-2xl text-lg text-center lg:text-left font-semibold">
+                WALK
+              </h2>
+              <p className="pt-5 lg:pt-10 lg:leading-10 leading-6 text-center lg:text-left lg:text-base text-sm">
+                サンプル文章が入ります
+                <br />
+                サンプル文章が入りますサンプル文章が入ります
+                <br />
+                サンプル文章が入りますサンプル文章が入ります
+                <br />
+                サンプル文章が入ります。
+              </p>
+            </div>
+            <img
+              src="/assets/hero/hero_background_1.png"
+              alt="sample"
+              className="size-full object-cover aspect-square"
+            />
+          </div>
+          <div className="grid w-full gap-[10vw] grid-cols-1 lg:grid-cols-2">
+            <div>
+              <h2 className="lg:text-2xl text-lg text-center lg:text-left font-semibold">
+                WALK
+              </h2>
+              <p className="pt-5 lg:pt-10 lg:leading-10 leading-6 text-center lg:text-left lg:text-base text-sm">
+                サンプル文章が入ります
+                <br />
+                サンプル文章が入りますサンプル文章が入ります
+                <br />
+                サンプル文章が入りますサンプル文章が入ります
+                <br />
+                サンプル文章が入ります。
+              </p>
+            </div>
+            <img
+              src="/assets/hero/hero_background_1.png"
+              alt="sample"
+              className="size-full object-cover aspect-square"
+            />
+          </div>
+          <div className="grid w-full gap-[10vw] grid-cols-1 lg:grid-cols-2">
+            <div>
+              <h2 className="lg:text-2xl text-lg text-center lg:text-left font-semibold">
+                WALK
+              </h2>
+              <p className="pt-5 lg:pt-10 lg:leading-10 leading-6 text-center lg:text-left lg:text-base text-sm">
+                サンプル文章が入ります
+                <br />
+                サンプル文章が入りますサンプル文章が入ります
+                <br />
+                サンプル文章が入りますサンプル文章が入ります
+                <br />
+                サンプル文章が入ります。
+              </p>
+            </div>
+            <img
+              src="/assets/hero/hero_background_1.png"
+              alt="sample"
+              className="size-full object-cover aspect-square"
+            />
+          </div>
+          <div className="grid w-full gap-[10vw] grid-cols-1 lg:grid-cols-2">
+            <div>
+              <h2 className="lg:text-2xl text-lg text-center lg:text-left font-semibold">
+                WALK
+              </h2>
+              <p className="pt-5 lg:pt-10 lg:leading-10 leading-6 text-center lg:text-left lg:text-base text-sm">
+                サンプル文章が入ります
+                <br />
+                サンプル文章が入りますサンプル文章が入ります
+                <br />
+                サンプル文章が入りますサンプル文章が入ります
+                <br />
+                サンプル文章が入ります。
+              </p>
+            </div>
+            <img
+              src="/assets/hero/hero_background_1.png"
+              alt="sample"
+              className="size-full object-cover aspect-square"
+            />
+          </div>
+          <div className="grid w-full gap-[10vw] grid-cols-1 lg:grid-cols-2">
+            <div>
+              <h2 className="lg:text-2xl text-lg text-center lg:text-left font-semibold">
+                WALK
+              </h2>
+              <p className="pt-5 lg:pt-10 lg:leading-10 leading-6 text-center lg:text-left lg:text-base text-sm">
                 サンプル文章が入ります
                 <br />
                 サンプル文章が入りますサンプル文章が入ります
