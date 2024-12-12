992518cb479217c9cba03f81329483a21877e938
Tahara Kazuki <>
2024-12-13 08:56:32 +0900

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import FadeAnimation from '@/components/FadeAnimation';
 import { NavProvider } from '@/providers/NavContext';
+import { Klee_One } from 'next/font/google';
+
+const Klee_one = Klee_One({ subsets: ['latin'], weight: ['400', '600'] });
 
 export const metadata: Metadata = {
   title: 'IXIA Group Home',
@@ -17,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja" className="h-screen">
-      <body className={`antialiased`}>
+      <body className={Klee_one.className}>
         <NavProvider>
           <Header />
           <FadeAnimation>{children}</FadeAnimation>
